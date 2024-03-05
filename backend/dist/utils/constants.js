42ec1617e897b280c2b68763445db24d269cf264
Tyler Hudson <tylerhudson96@gmail.com>
2024-03-05 17:10:39 -0500

    V1 finalized commit

--- backend/dist/utils/constants.js
+++
@@ -1,5 +0,0 @@
-export const COOKIE_NAME = "auth_token";
-export const COOKIE_LIFE = new Date();
-COOKIE_LIFE.setDate(COOKIE_LIFE.getDate() + 1);
-export const DOMAIN_NAME = "localhost";
-//# sourceMappingURL=constants.js.map
